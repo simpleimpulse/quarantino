959d67a4f4522a2b4917f377336bb716579b6227
Ani <anirudh.webdev@gmail.com>
2020-04-02 02:05:38 -0400

    Update favicon & Redirect to /quarantino

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.redirect('/quarantino')
+});
+router.get('/quarantino', function(req, res, next) {
   res.render('index', { title: '#Quarantino' });
 });
-
 module.exports = router;
